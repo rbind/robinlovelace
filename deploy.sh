fd47dc4a791f989b6271e9113981c6454bafc66b
Robin Lovelace <rob00x@gmail.com>
2020-04-05 13:13:40 +0100

    Update website

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 # Aim: update website
+
+# update source code
+git status
+git add -A
+git commit -am 'Update website'
+git push
+# update actual website
 cd robinlovelace.github.io
 ls
 git status
